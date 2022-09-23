9e35dbe6ddcc1f65c050075266afe10cb4c01e4d
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2022-09-23 13:07:07 -0300

    simple RBAC using casbin

---
+++ RBAC/main.py
@@ -0,0 +1,60 @@
+from ast import match_case
+from traceback import print_tb
+import casbin
+
+
+class User:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+    def create_user():
+        name = input("Name:\n> ")
+        name = name.lower()
+        name = name.strip()
+        role = None
+        while role not in {"1", "2", "professor", "student", "Professor", "Student"}:
+            role = input("Role:\n1. Professor\n2. Student\n> ")
+        if role == "1" or role == "Professor":
+            role = "professor"
+        if role == "2" or role == "Student":
+            role = "student"
+        new_user = User(name, role)
+        with open("policy.csv", "a") as csv_file:
+            csv_file.write(f"g, {new_user.name}, {new_user.role}\n")
+
+    def show_permissions(user):
+        enforcer = casbin.Enforcer("./model.conf", "./policy.csv")
+
+        sub = user.strip()  # the user that wants to access a resource.
+        obj = "data"  # the resource that is going to be accessed.
+        read = "read"  # the operation that the user performs on the resource.
+        write = "write"  # the operation that the user performs on the resource.
+
+        if enforcer.enforce(sub, obj, write):
+            print("This user has the following permissions: read, write")
+        elif enforcer.enforce(sub, obj, read):
+            print("This user has the following permissions: read")
+
+
+def menu():
+    print("=== MENU ===")
+    print("1. Create a new user")
+    print("2. Show user permissions")
+    option = None
+    while option not in {"1", "2"}:
+        option = input("> ")
+
+    match option:
+        case "1":
+            User.create_user()
+        case "2":
+            user = input(
+                "Type the name of the user you want to see the permissions:\n> "
+            )
+            User.show_permissions(user)
+
+
+if __name__ == "__main__":
+
+    menu()
